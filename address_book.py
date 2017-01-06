d2512795ee23f6883f329d67bc646edc66bf24db
Sergey731 <psv.731@yandex.ru>
2017-01-06 18:53:04 +0500

    add, remove & find

--- address_book.py
+++ address_book.py
@@ -1,11 +1,10 @@
 import sys
 
 filename = 'book.txt'
-find_name = str(sys.argv[2]) + ' ' + str(sys.argv[3])
 
 if sys.argv[1] == 'add':
-    name = find_name + str(sys.argv[4])
-    print('Added contact ' + '"' + find_name + ', ' + str(sys.argv[4]) + '"')
+    name = str(sys.argv[2]) + ' ' + str(sys.argv[3]) + str(sys.argv[4])
+    print('Added contact ' + '"' + str(sys.argv[2]) + ' ' + str(sys.argv[3]) + ', ' + str(sys.argv[4]) + '"')
 
     with open(filename, 'a') as file_object:
         file_object.write(name +'\n')
@@ -21,17 +20,34 @@ elif sys.argv[1] == 'remove':
                     book[line[:i]] = line[i:]
 
         for client_name in book.keys():
-            if client_name == find_name:
-                print("Removed contact " + '"' + find_name + ', ' + str(book[client_name]).strip() + '"')
+            if client_name == str(sys.argv[2]) + ' ' + str(sys.argv[3]):
+                print("Removed contact " + '"' + str(sys.argv[2]) + ' ' + str(sys.argv[3]) + ', ' + str(book[client_name]).strip() + '"')
                 del book[client_name]
                 break
         else:
-            print("Unknown contact " + '"' + find_name + '"')
+            print("Unknown contact " + '"' + str(sys.argv[2]) + ' ' + str(sys.argv[3]) + '"')
 
     with open(filename, 'w') as file_object:
         for key, value in book.items():
             file_object.write(key + value)
 
+elif sys.argv[1] == 'find':
+    with open(filename) as file_object:
+        lines = file_object.readlines()
+        book = {}
+
+    for line in lines:
+        for i in range(len(line)):
+            if line[i] == '+':
+                book[line[:i]] = line[i:]
+
+    for client_name in book.keys():
+        for part_name in client_name.split():
+            if part_name == str(sys.argv[2]):
+                print("Found for " + part_name + ':' + '\n\t- "' + client_name + ', ' + str(book[client_name]).strip() + '"')
+                break
+        else:
+            print("No results for " + '"' + str(sys.argv[2]) + '"')
 
 
 
