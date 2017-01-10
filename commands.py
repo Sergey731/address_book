9393203728b5d4a2fb5d07f42ca6c7e41068222b
Sergey731 <psv.731@yandex.ru>
2017-01-10 15:28:24 +0500

    request instead of first_name and no nimbers in {}

--- commands.py
+++ commands.py
@@ -5,45 +5,45 @@ filename = 'book.txt'
 
 
 def add(first_name, last_name, tel_number):
-    print('Added contact "{0} {1}, {2}"'.format(first_name, last_name, tel_number))
+    print('Added contact "{} {}, {}"'.format(first_name, last_name, tel_number))
     with open(filename, 'a') as f:
-        f.write('{0} {1};{2}\n'.format(first_name, last_name, tel_number))
+        f.write('{} {};{}\n'.format(first_name, last_name, tel_number))
 
 
 def remove(first_name, last_name):
     book = load_contacts_from_file(filename)
 
     for client_name in book.keys():
-        if client_name == '{0} {1}'.format(first_name, last_name):
-            print('Removed contact "{0} {1}, {2}"'.format(first_name, last_name, book[client_name]))
+        if client_name == '{} {}'.format(first_name, last_name):
+            print('Removed contact "{} {}, {}"'.format(first_name, last_name, book[client_name]))
             del book[client_name]
 
             with open(filename, 'w') as f:
                 for key, value in book.items():
-                    f.write('{0};{1}\n'.format(key, value))
+                    f.write('{};{}\n'.format(key, value))
 
             break
     else:
-        print('Unknown contact "{0} {1}"'.format(first_name, last_name))
+        print('Unknown contact "{} {}"'.format(first_name, last_name))
 
 
-def find(first_name):
+def find(request):
     book = load_contacts_from_file(filename)
 
     counter = 0
     for key, numbers in book.items(): # Поиск по значению - 999
-        if first_name in book[key]:
+        if request in book[key]:
             counter += 1
-            print('Found for "{0}":'.format(first_name))
-            print('\t- "{0}, {1}"'.format(key, numbers))
+            print('Found for "{}":'.format(request))
+            print('\t- "{}, {}"'.format(key, numbers))
             break
 
     for client_name in book.keys(): # Поиск по ключу - часть имени - John
-        if first_name in client_name.split():
-            print('Found for "{0}":'.format(first_name))
-            print('\t- "{0}, {1}"'.format(client_name, book[client_name]))
+        if request in client_name.split():
+            print('Found for "{}":'.format(request))
+            print('\t- "{}, {}"'.format(client_name, book[client_name]))
 
             break
-        elif first_name not in client_name.split() and counter == 0:
-            print('No results for "{0}"'.format(first_name))
+        elif request not in client_name.split() and counter == 0:
+            print('No results for "{}"'.format(request))
             break
