e56eb618b354186907497c052a553798cc832848
Sergey731 <psv.731@yandex.ru>
2017-01-17 19:06:42 +0500

    new class

--- models.py
+++ models.py
@@ -2,3 +2,7 @@ class Contact():
     def __init__(self, name, phone):
         self.name = name
         self.phone = phone
+
+    @property
+    def key(self):
+        return "{}_{}".format(self.name, self.phone)
---
+++ storage2.py
@@ -0,0 +1,48 @@
+from models import Contact
+
+
+class Storage:
+    # def add_contact(self, contact: Contact) -> bool:
+    def add_contact(self, contact):
+        raise NotImplementedError()
+
+    # def remove_contact(self, contact: Contact) -> bool:
+    def remove_contact(self, contact):
+        raise NotImplementedError()
+
+    # def find_contacts(self, query: str) -> List[Contact]:
+    def find_contacts(self, query):
+        raise NotImplementedError()
+
+    # def clear_contacts(self) -> bool:
+    def clear_contacts(self):
+        raise NotImplementedError()
+
+
+class MemoryStorage(Storage):
+    def __init__(self):
+        self.contacts = {};
+
+    def add_contact(self, contact):
+        self.contacts[contact.key] = contact
+        return True
+
+    def remove_contact(self, contact):
+        if contact.key in self.contacts:
+            del self.contacts[contact.key]
+            return True
+        return False
+
+    def find_contacts(self, query):
+        return [contact for key, contact in self.contacts.items() if query in key]
+
+    def clear_contacts(self):
+        self.contacts = {}
+
+
+class FileStorage(Storage):
+    pass
+
+
+class SqlStorage(Storage):
+    pass
