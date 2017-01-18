3418ac54ce4df0dac7becc71ef156411ca278596
Roman Paradeev <grumpy@yandex-team.ru>
2017-01-18 23:08:15 +0500

    switch tabs to spaces

--- test/test_storage2.py
+++ test/test_storage2.py
@@ -4,32 +4,32 @@ from storage2 import MemoryStorage
 from models import Contact
 
 '''
-	python3 -m unittest discover
+    python3 -m unittest discover
 '''
 class TestMemoryStorage(unittest.TestCase):
-	def test_creation(self):
-		storage = MemoryStorage()
+    def test_creation(self):
+        storage = MemoryStorage()
 
-		self.assertTrue(isinstance(storage, MemoryStorage))
-
-
-	def test_add_contact(self):
-		storage = MemoryStorage()
-		john = Contact('John Doe', '+7999')
-
-		storage.add_contact(john)
-		results = storage.find_contacts('John Doe')
-
-		self.assertEqual(len(results), 1)
-		self.assertEqual(john.name, results[0].name)
+        self.assertTrue(isinstance(storage, MemoryStorage))
 
 
-	def test_remove_contact(self):
-		pass
+    def test_add_contact(self):
+        storage = MemoryStorage()
+        john = Contact('John Doe', '+7999')
 
-	# дописать тесты для всех методов
-	# можно даже по несколько тестов на метод
+        storage.add_contact(john)
+        results = storage.find_contacts('John Doe')
+
+        self.assertEqual(len(results), 1)
+        self.assertEqual(john.name, results[0].name)
+
+
+    def test_remove_contact(self):
+        pass
+
+    # дописать тесты для всех методов
+    # можно даже по несколько тестов на метод
 
 
 if __name__ == '__main__':
-	unittest.main()
+    unittest.main()
